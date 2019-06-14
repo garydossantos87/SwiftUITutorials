d12651eece8bb44f9755e6f7cd06580533048a62
BeRepublic <mobile@berepublic.es>
2019-06-14 11:39:42 +0200

    Working with Hello World

---
+++ SwiftUITutorials/App/UI/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUITutorials
+//
+//  Created by BeiOSRepublic on 14/06/2019.
+//  Copyright © 2019 BeiOSRepublic. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
